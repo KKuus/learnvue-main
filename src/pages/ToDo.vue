2219b723427d0ab145ad58d8f00b1885a76ea52d
krsitistisr <kr.kuus@gmail.com>
2024-02-24 21:17:01 +0200

    i call this
    heh

--- src/pages/ToDo.vue
+++ src/pages/ToDo.vue
@@ -1,45 +1,54 @@
-<script setup>
-import { computed, reactive, ref } from 'vue';
+<script>
 import ItemList from '../components/ItemList.vue';
-const message = ref('');
-let i = 1;
-const items = reactive([
-  {id:i++,name:'leib', isDone: false },
-  {id:i++,name:'sai', isDone: true },
-  {id:i++,name:'piim', isDone: false },
-  {id:i++,name:'kommid', isDone: true },
-]);
-
-const doneItems = computed(() => {
-  return items.filter(item => item.isDone)
-});
-
-const toDoItems = computed(() => {
-  return items.filter(item => !item.isDone)
-});
-
-function add(){
-  if(message.value.trim() !== ''){
-    items.push({id:i++, name: message.value.trim(), isDone: false});
-  }
-  message.value = '';
-}
+let i = 0;
+export default {
+    components: {
+        ItemList
+    },
+    data(){
+        return {
+            message: '',
+            items: [
+                {id: i++, name: 'piim', isDone: true},
+                {id: i++, name: 'sai', isDone: false},
+                {id: i++, name: 'Viin', isDone: true},
+            ]
+        };
+    },
+    methods: {
+        add(){
+            if (this.message.trim() !== '') {
+                this.items.push({ id: i++, name: this.message.trim(), isDone: false });
+            }
+            this.message = '';
+        }
+    },
+    computed: {
+        doneItems(){
+            return this.items.filter(item => item.isDone);
+        },
+        toDoItems(){
+            return this.items.filter(item => !item.isDone);
+        }
+    }
+};
 </script>
 
 <template>
-  <div class="container">
-    <div class="field has-addons">
-      <p class="control is-expanded">
-        <input @keydown.enter="add" type="text" v-model="message" class="input">
-      </p>
-      <p class="control">
-        <button @click="add" class="button is-primary">Click me</button>
-      </p>
+    <div class="container">
+        <div class="field has-addons">
+            <p class="control is-expanded">
+                <input @keydown.enter="add" type="text" v-model="message" class="input">
+            </p>
+            <p class="control">
+                <button @click="add" class="button is-primary">Click me</button>
+            </p>
+        </div>
+        <div class="content">
+            <ItemList :items="items" title="All Items"></ItemList>
+            <ItemList :items="doneItems" title="Done Items"></ItemList>
+            <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
+        </div>
     </div>
-    <div class="content">
-      <ItemList :items="items" title="All Items"></ItemList>
-      <ItemList :items="doneItems" title="Done Items"></ItemList>
-      <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
-    </div>
-  </div>
 </template>
+
